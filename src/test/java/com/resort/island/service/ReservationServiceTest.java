adfa3fdad7501c3e10965eede048eaff616b86c7
Roman Novitski <roman.noviski@shorify.io>
2020-03-10 17:09:22 -0400

    Fixed mock data and tests

--- src/test/java/com/resort/island/service/ReservationServiceTest.java
+++ src/test/java/com/resort/island/service/ReservationServiceTest.java
@@ -39,24 +39,24 @@ class ReservationServiceTest {
                 "        \"email\": \"u1@u1\",\n" +
                 "        \"firstName\": \"u1\",\n" +
                 "        \"lastName\": \"u1\",\n" +
-                "        \"arrivalDate\": \"2020-10-01\",\n" +
-                "        \"departureDate\": \"2020-10-02\",\n" +
+                "        \"arrivalDate\": \"2020-04-01\",\n" +
+                "        \"departureDate\": \"2020-04-02\",\n" +
                 "        \"roomNumber\": 1\n" +
                 "    },\n" +
                 "    {\n" +
                 "        \"email\": \"u1@u1\",\n" +
                 "        \"firstName\": \"u1\",\n" +
                 "        \"lastName\": \"u1\",\n" +
-                "        \"arrivalDate\": \"2020-10-01\",\n" +
-                "        \"departureDate\": \"2020-10-03\",\n" +
+                "        \"arrivalDate\": \"2020-04-01\",\n" +
+                "        \"departureDate\": \"2020-04-03\",\n" +
                 "        \"roomNumber\": 2\n" +
                 "    },\n" +
                 "    {\n" +
                 "        \"email\": \"u1@u1\",\n" +
                 "        \"firstName\": \"u1\",\n" +
                 "        \"lastName\": \"u1\",\n" +
-                "        \"arrivalDate\": \"2020-10-05\",\n" +
-                "        \"departureDate\": \"2020-10-06\",\n" +
+                "        \"arrivalDate\": \"2020-04-05\",\n" +
+                "        \"departureDate\": \"2020-04-06\",\n" +
                 "        \"roomNumber\": 1\n" +
                 "    }\n" +
                 "]",
@@ -68,7 +68,7 @@ class ReservationServiceTest {
 
     @Test
     void getReservationsInRange() {
-        List<Reservation> result = reservationService.getReservationsInRange("2020-10-01", "2020-10-10");
+        List<Reservation> result = reservationService.getReservationsInRange("2020-04-01", "2020-04-10");
         Assert.assertEquals(reservations.size(), result.size());
         Assert.assertEquals(reservations.get(0).getId(), result.get(0).getId());
         Assert.assertEquals(reservations.get(1).getId(), result.get(1).getId());
@@ -77,31 +77,31 @@ class ReservationServiceTest {
 
     @Test
     void getAvailabilitiesInRange() {
-        List<Availability> result = reservationService.getAvailabilitiesInRange("2020-10-01", "2020-10-10");
+        List<Availability> result = reservationService.getAvailabilitiesInRange("2020-04-01", "2020-04-10");
         Assert.assertEquals(10, result.size());
 
-        Assert.assertEquals("2020-10-01", result.get(0).getDate());
+        Assert.assertEquals("2020-04-01", result.get(0).getDate());
         Assert.assertEquals(0, result.get(0).getNumberAvailableRooms().intValue());
 
-        Assert.assertEquals("2020-10-02", result.get(1).getDate());
+        Assert.assertEquals("2020-04-02", result.get(1).getDate());
         Assert.assertEquals(0, result.get(1).getNumberAvailableRooms().intValue());
 
-        Assert.assertEquals("2020-10-03", result.get(2).getDate());
+        Assert.assertEquals("2020-04-03", result.get(2).getDate());
         Assert.assertEquals(1, result.get(2).getNumberAvailableRooms().intValue());
 
-        Assert.assertEquals("2020-10-04", result.get(3).getDate());
+        Assert.assertEquals("2020-04-04", result.get(3).getDate());
         Assert.assertEquals(2, result.get(3).getNumberAvailableRooms().intValue());
 
-        Assert.assertEquals("2020-10-05", result.get(4).getDate());
+        Assert.assertEquals("2020-04-05", result.get(4).getDate());
         Assert.assertEquals(1, result.get(4).getNumberAvailableRooms().intValue());
 
-        Assert.assertEquals("2020-10-06", result.get(5).getDate());
+        Assert.assertEquals("2020-04-06", result.get(5).getDate());
         Assert.assertEquals(1, result.get(5).getNumberAvailableRooms().intValue());
 
-        Assert.assertEquals("2020-10-07", result.get(6).getDate());
+        Assert.assertEquals("2020-04-07", result.get(6).getDate());
         Assert.assertEquals(2, result.get(6).getNumberAvailableRooms().intValue());
 
-        Assert.assertEquals("2020-10-08", result.get(7).getDate());
+        Assert.assertEquals("2020-04-08", result.get(7).getDate());
         Assert.assertEquals(2, result.get(7).getNumberAvailableRooms().intValue());
     }
 
@@ -117,8 +117,8 @@ class ReservationServiceTest {
         reservation.setEmail("u2@u2");
         reservation.setFirstName("u2");
         reservation.setLastName("u2");
-        reservation.setArrivalDate("2020-10-04");
-        reservation.setDepartureDate("2020-10-06");
+        reservation.setArrivalDate("2020-04-04");
+        reservation.setDepartureDate("2020-04-06");
         String result = reservationService.createReservation(reservation);
         Assert.assertNotNull(result);
     }
@@ -129,8 +129,8 @@ class ReservationServiceTest {
         reservation.setEmail("u2@u2");
         reservation.setFirstName("u2");
         reservation.setLastName("u2");
-        reservation.setArrivalDate("2020-10-01");
-        reservation.setDepartureDate("2020-10-02");
+        reservation.setArrivalDate("2020-04-01");
+        reservation.setDepartureDate("2020-04-02");
         Assertions.assertThrows(IllegalStateException.class, () -> reservationService.createReservation(reservation))
                 .getMessage().equals("Unfortunately no rooms are available at the moment to make a reservation for the selected dates.");
     }
@@ -138,10 +138,10 @@ class ReservationServiceTest {
     @Test
     void updateReservation() {
         Reservation r = reservations.get(0);
-        Assert.assertEquals("2020-10-02", r.getDepartureDate());
-        r.setDepartureDate("2020-10-03");
+        Assert.assertEquals("2020-04-02", r.getDepartureDate());
+        r.setDepartureDate("2020-04-03");
         Reservation result = reservationService.updateReservation(r.getId(), r);
-        Assert.assertEquals("2020-10-03", result.getDepartureDate());
+        Assert.assertEquals("2020-04-03", result.getDepartureDate());
     }
 
     @Test
